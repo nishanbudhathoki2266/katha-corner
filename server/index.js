7e03e4265fd07bd85568b306c22c58bf8fe9cf28
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-08 07:22:14 +0545

    Added a server side code for the project

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.send(users);
 });
 
+app.get("users/:id", (req, res) => {
+  res.send(users.find((user) => user.id === +req.params.id));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
