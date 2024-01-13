90c483ec79b70a4bd64c26e004821e4a504f74c3
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-13 16:11:43 +0545

    Deleted all extra files and reconfigured the structure

--- server/src/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-const users = require("./../../configs/users.json");
-
-const getAllUsers = (req, res) => {
-  res.send(users);
-};
-
-module.exports = getAllUsers;
--- server/src/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const experss = require("express");
-const getAllUsers = require("./../controllers/userController");
-
-const router = experss.Router();
-
-router.get("/", getAllUsers);
-
-module.exports = router;
