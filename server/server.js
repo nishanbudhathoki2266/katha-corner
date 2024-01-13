0e34bda704443208697dfa7d0a596a8c812e9ce1
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-13 22:07:44 +0545

    Added an environment file to start working according to the dev or prod environment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 // Good practice to segregate server codes from main app or index file
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = require("./app");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
