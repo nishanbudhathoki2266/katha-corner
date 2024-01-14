39f926d450ab1d28af95c4a4020dcfd50affd230
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-14 08:01:41 +0545

    Setup Next UI in the client side.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextUIProvider from "./NextUIProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="dark text-foreground bg-background">
+          <NextUIProvider>{children}</NextUIProvider>
+        </main>
+      </body>
     </html>
   );
 }
