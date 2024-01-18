5aee67fb2e8313846218e79b0826598845987fa2
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-18 21:18:16 +0545

    Implemented a global error handling middleware

--- server/app.js
+++ server/app.js
@@ -21,4 +21,29 @@ app.use(express.static(`${__dirname}/public`));
 // ROUTES
 app.use("/api/v1/users", userRouter);
 
+// Handling unhandled routes
+app.all("*", (req, _, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  // If next function receives an argument, express will always assume that it is an error
+  // Skips all the middlewares in the middleware stack and send it straight to the error handler
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
