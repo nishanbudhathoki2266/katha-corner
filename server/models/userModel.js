e1842a21052b4070270560cb0d1fa601c28e2fa7
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-18 22:28:38 +0545

    Added a password hashing mechanism while signing up the user

---
+++ server/models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your full name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    // Transform the email into lowercase
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  image: String,
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+
+    // This only works on CREATE & SAVE!
+    validate: {
+      validator: function (value) {
+        return this.password === value;
+      },
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  // We only want to encrypt password while saving or password is modified
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // This is how we delete field from db! This field was just to make sure that the users entered two correct passwords
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
