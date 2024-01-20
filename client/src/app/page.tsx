17f7b380ff63bfa12164606108e2f2244af6b9a1
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-01-20 22:29:24 +0545

    Added a basic login page (just a template for now)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const HomePage = () => {
+  const router = useRouter();
+
+  router.push("/auth/sign-up");
   return <h1>WELCOME TO KATHA CORNER</h1>;
 };
 
