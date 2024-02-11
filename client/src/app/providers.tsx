dbfc9cd683ac187c33527f7d939eb58ec6da33c2
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-02-11 15:07:12 +0545

    Setup and Initialized React Query for using a better way of fetching!

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Provider } from "react-redux";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
@@ -6,17 +7,25 @@ import { useRouter } from "next/navigation";
 import { persistor, store } from "@/redux/store";
 import { NextUIProvider } from "@nextui-org/react";
 import { PersistGate } from "redux-persist/integration/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
+  const queryClient = new QueryClient();
+
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NextUIProvider navigate={router.push}>
-          <NextThemesProvider attribute="class" defaultTheme="light">
-            {children}
-          </NextThemesProvider>
-        </NextUIProvider>
-      </PersistGate>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <PersistGate loading={null} persistor={persistor}>
+          <NextUIProvider navigate={router.push}>
+            <NextThemesProvider attribute="class" defaultTheme="light">
+              {children}
+            </NextThemesProvider>
+          </NextUIProvider>
+        </PersistGate>
+      </QueryClientProvider>
     </Provider>
   );
 }
